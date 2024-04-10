b0ec67bd8f433ed350ba6da04ec1d631c0fa030d
jpnsantoss <jpnsantos14@gmail.com>
2024-04-10 16:36:06 +0100

    database class

--- config/constants.php
+++ config/constants.php
@@ -6,6 +6,8 @@ define('URLROOT', 'http://localhost:8000');
 // Site Name
 define('SITENAME', 'LWT Marketplace');
 
+define('DATABASE_NAME', 'marketplace.db');
+
 // CRUD Operations' Alert Messages
 define('TASK_NOT_CREATED', 'Something went wrong creating new task');
 define('TASK_NOT_UPDATED', 'Task updated successfully');
