2eba0e2ed846f2777468499c46cb09eeffc23a29
jpnsantoss <jpnsantos14@gmail.com>
2024-04-11 15:50:10 +0100

    navbar prototype

--- src/views/Home/index.php
+++ src/views/Home/index.php
@@ -1,3 +1,4 @@
 <?php require_once APPROOT . '/src/views/Layout/header.php'; ?>
+<?php require_once APPROOT . '/src/views/Layout/navbar.php'; ?>
 <p>Home</p>
 <?php require_once APPROOT . '/src/views/Layout/footer.php'; ?>
---
+++ src/views/Layout/navbar.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="navbar">
+        <ul class="nav">
+            <li><a href="#home">LTW Marketplace</a></li>
+        </ul>
+        <button class="icon-button dropdown"><i class="icon">menu</i></button>
+        <div class="nav-collapse">
+            <ul class="nav nav-right">
+                <li><a href="#"><i class="icon left">chat_bubble</i></a></li>
+                <li><a href="#"><i class="icon left">favorite</i></a></li>
+                <li><a href="#"><i class="icon left">person</i></a></li>
+                <li><a href="#">John Smith</a></li>
+                <li><a href="#" class="highlight">Vender</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
