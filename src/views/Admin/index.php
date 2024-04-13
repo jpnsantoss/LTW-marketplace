da13f418ce91e4846fdff8ca2893a1ab09ef6df8
jpnsantoss <jpnsantos14@gmail.com>
2024-04-13 18:40:09 +0100

    crud conditions and sizes

--- src/views/Admin/index.php
+++ src/views/Admin/index.php
@@ -1,3 +1,4 @@
+<h1>Categories</h1>
 <!-- create category -->
 <form action="<?= URLROOT; ?>/category/create" method="post">
     <label for="name">Category Name</label>
@@ -13,6 +14,51 @@
             <?= $category->name; ?> &emsp;
 
             <form action="<?= URLROOT; ?>/category/<?= $category->id; ?>/delete" method="post">
+                <input type="submit" value="Delete">
+            </form>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+<h1>Sizes</h1>
+
+<!-- create size -->
+<form action="<?= URLROOT; ?>/size/create" method="post">
+    <label for="name">Size Name</label>
+    <input type="text" name="name" id="name">
+    <button type="submit">Add Size</button>
+</form>
+
+<!-- list sizes -->
+<ul>
+    <?php foreach ($data["sizes"] as $size) : ?>
+        <li>
+            <?= $size->name; ?> &emsp;
+
+            <form action="<?= URLROOT; ?>/size/<?= $size->id; ?>/delete" method="post">
+                <input type="submit" value="Delete">
+            </form>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
+<h1>Conditions</h1>
+
+<!-- create condition -->
+<form action="<?= URLROOT; ?>/condition/create" method="post">
+    <label for="name">Condition Name</label>
+    <input type="text" name="name" id="name">
+    <button type="submit">Add Condition</button>
+</form>
+
+<!-- list conditions -->
+
+<ul>
+    <?php foreach ($data["conditions"] as $condition) : ?>
+        <li>
+            <?= $condition->name; ?> &emsp;
+
+            <form action="<?= URLROOT; ?>/condition/<?= $condition->id; ?>/delete" method="post">
                 <input type="submit" value="Delete">
             </form>
         </li>
