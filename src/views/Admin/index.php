87242978c01113feab5e50de49417d3e83791b34
jpnsantoss <jpnsantos14@gmail.com>
2024-04-14 11:49:55 +0100

    add and delete items :)

--- src/views/Admin/index.php
+++ src/views/Admin/index.php
@@ -1,3 +1,53 @@
+<h1>Items</h1>
+
+<!-- create item -->
+
+<form action="<?= URLROOT; ?>/item/create" method="post">
+    <label for="brand">Brand</label>
+    <input type="text" name="brand" id="brand">
+    <label for="model">Model</label>
+    <input type="text" name="model" id="model">
+    <label for="price">Price</label>
+    <input type="number" name="price" id="price">
+    <label for="category">Category</label>
+    <select name="category" id="category">
+        <?php foreach ($data["categories"] as $category) : ?>
+            <option value="<?= $category->id; ?>"><?= $category->name; ?></option>
+        <?php endforeach; ?>
+    </select>
+    <label for="size">Size</label>
+    <select name="size" id="size">
+        <?php foreach ($data["sizes"] as $size) : ?>
+            <option value="<?= $size->id; ?>"><?= $size->name; ?></option>
+        <?php endforeach; ?>
+    </select>
+    <label for="condition">Condition</label>
+    <select name="condition" id="condition">
+        <?php foreach ($data["conditions"] as $condition) : ?>
+            <option value="<?= $condition->id; ?>"><?= $condition->name; ?></option>
+        <?php endforeach; ?>
+    </select>
+    <button type="submit">Add Item</button>
+</form>
+
+<!-- list items -->
+
+<ul>
+    <?php foreach ($data["items"] as $item) : ?>
+        <li>
+            <?= $item->name; ?> &emsp;
+            <?= $item->price; ?> &emsp;
+            <?= $item->category_name; ?> &emsp;
+            <?= $item->size_name; ?> &emsp;
+            <?= $item->condition_name; ?> &emsp;
+
+            <form action="<?= URLROOT; ?>/item/<?= $item->id; ?>/delete" method="post">
+                <input type="submit" value="Delete">
+            </form>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
 <h1>Categories</h1>
 <!-- create category -->
 <form action="<?= URLROOT; ?>/category/create" method="post">
