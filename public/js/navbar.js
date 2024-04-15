50fa3686bbc1d61fac1fea4a81923515b93be7f8
jpnsantoss <jpnsantos14@gmail.com>
2024-04-15 15:25:32 +0100

    account dropdown

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,9 +2,27 @@ document.getElementById('menu-button').addEventListener('click', function () {
     var menuLinks = document.getElementById('menu-links');
     if (menuLinks.style.opacity === '0') {
         menuLinks.style.opacity = '1';
-        menuLinks.style.maxHeight = '500px'; // Adjust this value as needed
+        menuLinks.style.maxHeight = '600px'; // Adjust this value as needed
     } else {
         menuLinks.style.opacity = '0';
         menuLinks.style.maxHeight = '0';
+    }
+});
+
+document.querySelector('.dropdown').addEventListener('click', function (event) {
+    event.stopPropagation();
+    this.querySelector('ul').classList.toggle('show');
+});
+
+// Close the dropdown menu if the user clicks outside of it
+window.addEventListener('click', function (event) {
+    if (!event.target.matches('.dropdown')) {
+        var dropdowns = document.querySelectorAll('.dropdown ul');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
 });
--- src/views/Common/common.php
+++ src/views/Common/common.php
@@ -28,8 +28,23 @@ function getNavbar()
                 <ul>
                     <li><a href="#"><i class="icon">chat_bubble</i> Messages</a></li>
                     <li><a href="#"><i class="icon">favorite</i> Wishlist</a></li>
-                    <li><a href="#"><i class="icon">person</i> Account</a></li>
-                    <li><a href="#" class="highlight">Post Items</a></li>
+
+
+                    <li class="dropdown">
+                        <a href="#"><i class="icon">person</i> Account</a>
+                        <ul>
+                            <li><a href="#">Profile</a></li>
+                            <li><a href="#">Settings</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </li>
+
+
+                    <?php if (isSeller()) : ?>
+                        <li><a href="#" class="highlight">Post Items</a></li>
+                    <?php else : ?>
+                        <li><a href="#" class="highlight">Become a Seller</a></li>
+                    <?php endif; ?>
                 </ul>
             </div>
         </nav>
